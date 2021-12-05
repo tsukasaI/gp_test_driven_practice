70b5f43fcecc456a50e860c649e45263a725b96c
tsukasaI <inoue.tsukasa.4@gmail.com>
2021-12-05 16:23:09 +0900

    assert error

---
+++ pointer/pointer.go
@@ -0,0 +1,26 @@
+package pointer
+
+import "fmt"
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
+}
+
+func (w Wallet) Balance() Bitcoin {
+	return w.balance
+}
