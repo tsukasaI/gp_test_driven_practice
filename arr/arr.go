be683414de6b6af9e334a7c9f0b85ea6b43c50b2
tsukasaI <inoue.tsukasa.4@gmail.com>
2021-12-05 14:05:17 +0900

    slice test

--- arr/arr.go
+++ arr/arr.go
@@ -7,3 +7,23 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersToTails ...[]int) []int {
+	var tails []int
+	for _, numbers := range numbersToTails {
+		if len(numbers) == 0 {
+			tails = append(tails, 0)
+		} else {
+			tail := numbers[1:]
+			tails = append(tails, Sum(tail))
+		}
+	}
+	return tails
+}
