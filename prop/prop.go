81c0b5303f4402edc2f2c829cb1e51eb09d5ae62
tsukasaI <inoue.tsukasa.4@gmail.com>
2021-12-12 07:29:59 +0900

    to arabic til 4

--- prop/prop.go
+++ prop/prop.go
@@ -7,7 +7,7 @@ type RomanNumeral struct {
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumeral{
+var allRomanNumerals = RomanNumerals{
 	{1000, "M"},
 	{900, "CM"},
 	{500, "D"},
@@ -34,4 +34,45 @@ func ConvertToRoman(arabic int) string {
 	}
 
 	return result.String()
+}
+
+type RomanNumerals []RomanNumeral
+
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+	return 0
+}
+
+func ConvertToArabic(roman string) int {
+	total := 0
+
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		if couldBeSubtractive(i, symbol, roman) {
+			nextSymbol := roman[i+1]
+
+			potentialNumber := string([]byte{symbol, nextSymbol})
+
+			value := allRomanNumerals.ValueOf(potentialNumber)
+
+			if value != 0 {
+				total += value
+				i++
+			} else {
+				total++
+			}
+		} else {
+			total++
+		}
+	}
+	return total
+}
+
+func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
+	return index+1 < len(roman) && currentSymbol == 'I'
 }
